7ad742daa84ab44d96c877ae5f6a5bd2ea04babb
rafikoding <rafikindnes1343@gmail.com>
2022-10-15 19:23:04 +0700

    first commit

---
+++ Sesi4.py
@@ -0,0 +1,42 @@
+#ctype_data_int
+pertama = 20
+print(pertama)
+print(type(pertama))
+
+#type_data_float
+kedua = 3.5
+print(kedua)
+print(type(kedua))
+
+#type_data_tuple
+ketiga = 6, 9
+print(ketiga)
+print(type(ketiga))
+
+#type_data_String
+keempat = "ayo makan"
+print(keempat)
+print(type(keempat))
+
+#type_data_boolean
+angka1 = 20
+angka2 = 15
+penjumlahan = angka1 + angka2
+pengurangan  = angka1 - angka2
+print(penjumlahan < pengurangan)
+print(type(penjumlahan < pengurangan))
+
+#type_data_complex
+B2 = complex(6, 5)
+print(B2)
+print(type(B2))
+
+#type_data_list
+Al = ([1,2,3,4])
+print(Al)
+print(type(Al))
+
+#type_data_dictionary
+Ping = {"nama":"Rafi", 'kelas': 12}
+print(Ping)
+print(type(Ping))
